4190211f3e983a165db0866f1d10690af3944ae7
Martin Komischke <martin@6wunderkinder.com>
2013-06-11 08:58:37 +0200

    Prepared basic structs and some functions.

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Spoon struct {
+	Id int
+}
+
+func TryPickUp(s *Spoon) bool {
+	return true
+}
+
+func PickUp(s *Spoon) bool {
+	return true
+}
+
+func PutDown(s *Spoon) bool {
+	return true
+}
+
+type Philosoph struct {
+	Id int
+	Left *Spoon
+	Right *Spoon
+	Consumed int
+}
+
+func InitialzePhilosoph(p *Philosoph, l *Spoon, r *Spoon)  {
+}
 
 func main() {
-	fmt.Printf("hello world!")
+	var spoons [5] Spoon
+	var philosophs [5] Philosoph
+	
+	for i := 0; i < 5; i++ {
+		spoons[i] = Spoon { Id:i }
+		philosophs[i] = Philosoph { Id: i }
+		
+		if (i == 5)
+			IntializePhilosoph(&philosophs[i], &spoons[i], &spoons[0])
+		else
+			IntializePhilosoph(&philosophs[i], &spoons[i], &spoons[i + 1])
+	}
+	
+	for i := 0; i < 5; i++ {
+		fmt.Printf("\nSpoon: %+v", spoons[i].Id)
+	}
+	
+	for i := 0; i < 5; i++ {
+		//p := &philosophs[i]
+		//fmt.Printf("\nLeft spoon: %+v  > Philosoph: %+v < Right spoon: %+v", p.Left.Id, p.Id, p.Right.Id)
+	}
 }
